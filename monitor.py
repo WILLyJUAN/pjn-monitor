447bc5a66550e37fb0c7592dcf89df606d18fba8
WILLyJUAN <williamsmit4x@gmail.com>
2026-06-23 00:42:58 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,12 +4,13 @@ import requests
 BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-message = "Тестовое сообщение от PJN Monitor"
-
-requests.post(
+r = requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     json={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": "Тест PJN Monitor"
     }
 )
+
+print(r.status_code)
+print(r.text)
