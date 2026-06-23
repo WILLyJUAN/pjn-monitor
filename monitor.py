06a7ac92d5e8571396b00996a8ade9c00bfe2a7c
WILLyJUAN <williamsmit4x@gmail.com>
2026-06-23 02:02:57 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,10 +1,18 @@
+import os
 import requests
 
-r = requests.get(
-    "https://portalpjn.pjn.gov.ar/inicio",
+user = os.environ["PJN_USER"]
+password = os.environ["PJN_PASSWORD"]
+
+s = requests.Session()
+
+r = s.get(
+    "https://sso.pjn.gov.ar/auth/realms/pjn/protocol/openid-connect/auth?client_id=pjn-portal",
+    allow_redirects=True,
     timeout=30
 )
 
 print("STATUS:", r.status_code)
 print("URL:", r.url)
-print("LENGTH:", len(r.text))
+print("LOGIN PAGE FOUND:", "username" in r.text.lower())
+print("PASSWORD FIELD FOUND:", "password" in r.text.lower())
